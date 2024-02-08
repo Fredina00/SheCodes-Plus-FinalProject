9d6db2259d3d1a6c69dc330481b5dc49fa2a2a2c
Fredina00 <152204723+Fredina00@users.noreply.github.com>
2024-02-08 20:14:17 +0000

    added weather forecast html with dummy content

---
+++ src/index.js
@@ -0,0 +1,60 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#weather-icon");
+
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+}
+
+function formatDate(date) {
+  let minute = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sunday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minute < 10) {
+    minutes = `0${minute}`;
+  }
+
+  return `${day} ${hours}:${minute}`;
+}
+
+function searchCity(city) {
+  let apiKey = "2ocb7acd896tf43b0e5f09484729f44d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector(".search-form");
+searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Accra");
